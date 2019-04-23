872d2f9d16e64b43cd3e71f47eca138604557c0e
hk1510 <hkthetinkerer@gmail.com>
2019-04-23 15:06:25 -0700

    inital bot update

--- bot.py
+++ bot.py
@@ -1,1 +1,32 @@
+import discord
+from discord.ext.commands import Bot
 
+TOKEN = 'NTcwMzYyNjU4MzQwMjA4NjQx.XL-IkA.kbBohf6fWHW4Tph7rvkGTZzlxwY'
+BOT_PREFIX = "bb$"
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------------------------')
+
+@client.event
+async def on_message(message):
+
+    if client.user.mentioned_in(message) and ("@everyone" not in message.content) and ("@here" not in message.content):
+        await channel.send("Hi!")
+
+    await client.process_commands(message)
+
+@client.command(pass_context = True)
+async def ping(context):
+    awair channel.send('pong!')
+
+
+client.run(TOKEN)
+
+client.loop.run_until_complete(client.logout())
